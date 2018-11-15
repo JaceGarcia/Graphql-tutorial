0ec1a9652ac897e8a0cf25017962d562ddc0c24d
JaceGarcia <jaces.place@gmail.com>
2018-11-15 13:55:27 -0500

    added messages

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
-
+const crypto = require('crypto')
 
 const db = {
     users: [
-        { id: '1', email: 'alex@gmail.com', name: 'Alex'},
-        { id: '2', email: 'max@gmail.com', name: 'Max'}
+        { id: '1', email: 'alex@gmail.com', name: 'Alex', avatarUrl: 'https://en.gravatar.com/'},
+        { id: '2', email: 'max@gmail.com', name: 'Max', avatarUrl: 'https://en.gravatar.com/'}
+    ],
+    messages: [
+        {id: '1', userId: '1', body: 'Hello', createdAt: Date.now()}
+        ,
+        {id: '2', userId: '2', body: 'Hi', createdAt: Date.now()}
+        ,
+        {id: '3', userId: '1', body: 'whats up', createdAt: Date.now()}
     ]
 }
+
 
 const schema = buildSchema(`
     type Query {
         users: [User!]!
+        user(id: ID!): User
+        messages: [Message !]!
     }
-
+    type Mutation {
+        addUser(email: String!, name: String): User
+    }
     type User {
         id: ID!
         email: String!
         name: String
         avatarUrl: String
+        messages: [Message !]!
+
+    }
+    type Message {
+        id: ID!
+        body: String!
+        createdAt: String!
     }
 `)
 
 const rootValue = {
-    users: () => db.users
+    users: () => db.users,
+    user: args => db.users.find(user => user.id === args.id),
+    messages: () => db.messages,
+    addUser: ({email, name}) => {
+        const user = {
+            id: crypto.randomBytes(10).toString('hex'),
+            email,
+            name
+        }
+
+        db.users.push(user)
+
+        return user
+    }
 }
 const app = express()
 
@@ -37,24 +69,4 @@ app.use('/graphql', graphqlHTTP({
 app.listen(3000, () => console.log('listening on 3000'))
 
 
-// const { graphql, buildSchema } = require(`graphql`)
-
-
-
-// graphql(
-//     schema,
-//     `
-//         {
-//             users {
-//                 id
-//                 email
-//             }
-//         }
-//     `,
-//     rootValue
-// ).then(
-//     res => console.dir(res, { depth: null})
-// ).catch(
-//     console.error
-// )
 
