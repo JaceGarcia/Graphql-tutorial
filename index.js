5acebb81716a58575b366b47b58157a1c07c5bf4
JaceGarcia <jaces.place@gmail.com>
2018-11-15 15:35:30 -0500

    updated code to run appolo server and have it woring with graphql playground

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const { buildSchema } = require('graphql')
+const {ApolloServer, gql} = require('apollo-server')
+
 const crypto = require('crypto')
 
 const db = {
@@ -17,16 +16,8 @@ const db = {
     ]
 }
 
-class User {
-    constructor (user) {
-        Object.assign(this.user)
-    }
-    get messages () {
-        return db.messages.filter(messages => message.userId === this.id)
-    }
-}
 
-const schema = buildSchema(`
+const typeDefs = gql`
     type Query {
         users: [User!]!
         user(id: ID!): User
@@ -48,33 +39,33 @@ const schema = buildSchema(`
         body: String!
         createdAt: String!
     }
-`)
+`
 
-const rootValue = {
-    users: () => db.users.map(user => new User(user)),
-    user: args => db.users.find(user => user.id === args.id),
-    messages: () => db.messages,
-    addUser: ({email, name}) => {
-        const user = {
-            id: crypto.randomBytes(10).toString('hex'),
-            email,
-            name
-        }
 
-        db.users.push(user)
-
-        return user
+const resolvers = {
+    Query: {
+        users: () => db.users,
+        user: (root, {id}) => db.users.find(user => user.id === args.id),
+        messages: () => db.messages,
+    },
+    Mutation: {
+        addUser: ( root, {email, name }) => {
+            const user = {
+              id: crypto.randomBytes(10).toString('hex'),
+              email,
+              name
+            }
+        
+            db.users.push(user)
+        
+            return user
+        }    
+    },
+    User: {
+        messages: user => db.messages.filter(messages => message.userId === user.id)
     }
 }
-const app = express()
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true
-}))
+const server = new ApolloServer({ typeDefs, resolvers})
 
-app.listen(3000, () => console.log('listening on 3000'))
-
-
-
+server.listen().then(({url}) => console.log(url))
